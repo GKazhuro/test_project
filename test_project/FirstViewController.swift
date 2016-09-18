69d72b1df38b3202e4e0bb5f0f7799af916d28ec
Георгий Кажуро <gkazhuro@imac-georgij.local>
2016-09-18 15:38:44 +0300

    Initial Commit

---
+++ test_project/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  test_project
+//
+//  Created by Георгий Кажуро on 18.09.16.
+//  Copyright © 2016 Георгий Кажуро. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ test_project/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  test_project
+//
+//  Created by Георгий Кажуро on 18.09.16.
+//  Copyright © 2016 Георгий Кажуро. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
